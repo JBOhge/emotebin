baa4918d04bf7263b02e13e946fbbd3289aa7054
Joel.Ohge <Joel.Ohge@cognizant.com>
2022-04-27 15:32:49 -0500

    updated emoteComponent and homeComponent

--- src/app/components/emote/emote.component.ts
+++ src/app/components/emote/emote.component.ts
@@ -10,7 +10,7 @@ import { emoteList } from '../../emoteData';
 })
 export class EmoteComponent implements OnInit {
   
-  @Input() emote:Emote = emoteList[1];
+  @Input() emote:Emote = emoteList[3];
 
   constructor() {}
 
